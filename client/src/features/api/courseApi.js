b44bff9199b18337732ff6080e9de84260faf754
Sadiq0909 <8604sadiq@gmail.com>
2025-01-29 14:55:56 +0530

    Minor changes in createCourse and getCreatorCourses Api

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const COURSE_API = "http://localhost:8080/api/v1/course"
 
 export const courseApi = createApi({
+    tagTypes : ["Refetch_Creator_Course"] , 
     reducerPath : "courseApi",
     baseQuery:fetchBaseQuery({
         baseUrl : COURSE_API,
@@ -13,13 +14,15 @@ export const courseApi = createApi({
                 url : "",
                 method : "POST",
                 body : {courseTitle , category}
-            })
+            }),
+            invalidatesTags : ["Refetch_Creator_Course"]
         }),
         getCreatorCourse :builder.query({
             query:()=>({
                 url : "" ,
                 method :"GET"
-            })
+            }),
+            providesTags : ["Refetch_Creator_Course"]
         })
     })
 })
