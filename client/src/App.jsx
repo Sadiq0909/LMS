5145be903dfa6692085bc4ea55a64feaaaf37304
Sadiq0909 <8604sadiq@gmail.com>
2025-01-30 10:17:58 +0530

    Add Skeletopn for the create lecture page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Dashboard from './pages/admin/Dashboard'
 import CourseTable from './pages/admin/course/CourseTable'
 import AddCourse from './pages/admin/course/AddCourse'
 import EditCourse from './pages/admin/course/EditCourse'
+import CreateLecture from './pages/admin/lecture/CreateLecture'
 
 const appRouter = createBrowserRouter([
   {
@@ -55,6 +56,10 @@ const appRouter = createBrowserRouter([
             path:"course/:courseId",
             element :<EditCourse/>
           },
+          {
+            path : "course/:courseId/lecture",
+            element : <CreateLecture />
+          }
         ]
       }
     ]
