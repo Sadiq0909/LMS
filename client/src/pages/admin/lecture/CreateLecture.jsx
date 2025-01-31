a2763c56326e37d8ee6937593e24fe18f63302f6
Sadiq0909 <8604sadiq@gmail.com>
2025-01-31 18:58:14 +0530

    Add Lecture model

--- client/src/pages/admin/lecture/CreateLecture.jsx
+++ client/src/pages/admin/lecture/CreateLecture.jsx
@@ -11,16 +11,25 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Loader2 } from "lucide-react";
 import { useCreateCourseMutation } from "@/features/api/courseApi";
 import { toast } from "sonner";
 const CreateLecture = () => {
+    const params = useParams();
+    const courseId = params.courseId ; 
+    const [lectureTitle , setLectureTitle] = useState("")
     const isLoading = false
+    const navigate = useNavigate()
+
+    const createLectureHandler = async()=>{
+        
+    }
+
   return (
     <div className="flex-1 mx-10">
             <div className="mb-4">
-                <h1 className="font-bold text-xl">Let's add course and basic course details for your new Course</h1>
+                <h1 className="font-bold text-xl">Let's add Lectures and basic course details for your new lecture</h1>
                 <p className="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, rerum?</p>
             </div>
             <div className="space-y-4">
@@ -28,42 +37,15 @@ const CreateLecture = () => {
                     <Label>Title</Label>
                     <Input 
                     type="text"
-                    placeholder="Your course name" />
-                </div>
-                <div>
-                    <Label>Category</Label>
-                    <Select >
-                        <SelectTrigger className="w-[180px]">
-                            <SelectValue placeholder="Select a category" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectLabel>Category</SelectLabel>
-                                <SelectItem value="Next JS">Next JS</SelectItem>
-                                <SelectItem value="Data Science">Data Science</SelectItem>
-                                <SelectItem value="Frontend Development">
-                                    Frontend Development
-                                </SelectItem>
-                                <SelectItem value="Fullstack Development">
-                                    Fullstack Development
-                                </SelectItem>
-                                <SelectItem value="MERN Stack Development">
-                                    MERN Stack Development
-                                </SelectItem>
-                                <SelectItem value="Javascript">Javascript</SelectItem>
-                                <SelectItem value="Python">Python</SelectItem>
-                                <SelectItem value="Docker">Docker</SelectItem>
-                                <SelectItem value="MongoDB">MongoDB</SelectItem>
-                                <SelectItem value="HTML">HTML</SelectItem>
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
+                    value ={lectureTitle}
+                    onChange = {(e)=>{setLectureTitle(e.target.value)}}
+                    placeholder="Your title name" />
                 </div>
                 <div className="flex gap-4 mt-5 items-center">
-                    <Button variant="outline" onClick={() => navigate("/admin/course")}>Back</Button>
-                    <Button disabled = {isLoading}  >
+                    <Button variant="outline" onClick={() => navigate(`/admin/course/:${courseId}`)}>Back to Course</Button>
+                    <Button disabled = {isLoading}  onClick = {createLectureHandler}>
                         {
-                            isLoading ? <><Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please Wait</>:"Create"
+                            isLoading ? <><Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please Wait</>:"Create Lecture"
                         }
                     </Button>
                 </div>
