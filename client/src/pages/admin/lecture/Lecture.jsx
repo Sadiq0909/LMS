5556fbe142a7659856c1f0eb7d5960b51284640c
Sadiq0909 <8604sadiq@gmail.com>
2025-02-01 20:26:33 +0530

    Add Frontend for update Lecture and lecture Tab

--- client/src/pages/admin/lecture/Lecture.jsx
+++ client/src/pages/admin/lecture/Lecture.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Edit} from 'lucide-react'
+import { Edit } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 
-const Lecture = ({lecture , index , courseId}) => {
+const Lecture = ({ lecture, index, courseId }) => {
   const navigate = useNavigate()
-  const goToUpdateLecture = ()=>{
-    navigate(`/${lecture._id}`)
+  const goToUpdateLecture = () => {
+    navigate(`${lecture._id}`)
   }
   return (
     <div className='flex items-center justify-between bg-zinc-200 dark:bg-zinc-900 px-4 py-2 rounded-md my-2'>
-      <h1 className='font-bold text-zinc-900 dark:text-zinc-200'>Lecture - {index+1} : {lecture.lectureTitle}</h1>
+      <h1 className='font-bold text-zinc-900 dark:text-zinc-200'>Lecture - {index + 1} : {lecture.lectureTitle}</h1>
       <Edit
-      className='cursor-pointer text-zinc-700 dark:text-zinc-300 hover:text-pink-600 dark:hover:text-pink-400'
-      onClick={goToUpdateLecture}
-       />
+        className='cursor-pointer text-zinc-700 dark:text-zinc-300 hover:text-pink-600 dark:hover:text-pink-400'
+        onClick={goToUpdateLecture}
+      />
     </div>
   )
 }
---
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -0,0 +1,50 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+import React from 'react'
+
+const LectureTab = () => {
+    return (
+        <Card>
+            <CardHeader className="flex justify-between">
+                <div>
+                    <CardTitle>Edit Lecture</CardTitle>
+                    <CardDescription>Make changes and click save when done</CardDescription>
+                </div>
+            </CardHeader>
+            <CardContent>
+                <div>
+                    <Label>Title</Label>
+                    <Input
+                        type="text"
+                        placeholder="Title of Lecture"
+                    />
+                </div>
+                <div>
+                    <Label>Video <span className='text-red-500'>*</span></Label>
+                    <Input
+                        type="file"
+                        accept="video/*"
+                        className="w-fit"
+                    />
+                </div>
+                <div className="flex items-center space-x-3 my-5">
+                    <Switch id="free" />
+                    <Label htmlFor="free">Is this video Free ?</Label>
+                </div>
+                <div className='mt-4 flex space-x-2'>
+                    <Button>
+                        Remove Lecture
+                    </Button>
+                    <Button variant = "outline">
+                        Update Lecture
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default LectureTab
